d6df02848deace957015748942901ab478483163
alexlibe95 <alexlibe95@gmail.com>
2020-01-15 22:54:11 +0200

    Adding Auto-Location via GPS Cooooool

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Capacitor, Plugins} from '@capacitor/core'
 
 import { AuthService } from './auth/auth.service';
 
@@ -17,8 +16,6 @@ export class AppComponent {
 
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
     private authService: AuthService,
     private router: Router
   ) {
@@ -27,8 +24,9 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      if (Capacitor.isPluginAvailable('SplashScreen')) {
+        Plugins.SplashScreen.hide();
+      }
     });
   }
 
--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ActionSheetController, AlertController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
 import { map, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { Capacitor, Plugins} from '@capacitor/core'
 
 import { environment } from '../../../../environments/environment';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
-import { PlaceLocation } from '../../../places/location.model';
-import { of } from 'rxjs';
+import { PlaceLocation, Coordinates } from '../../../places/location.model';
 
 @Component({
   selector: 'app-location-picker',
@@ -20,12 +21,63 @@ export class LocationPickerComponent implements OnInit {
 
   constructor(
     private modalCtrl: ModalController,
-    private http: HttpClient
+    private http: HttpClient,
+    private actionSheetCtrl: ActionSheetController,
+    private alertCtrl: AlertController
   ) { }
 
   ngOnInit() {}
 
   onPickLocation() {
+    this.actionSheetCtrl.create(
+      {
+        header: 'Please choose between the 2 options',
+        buttons: [
+          {
+            text: 'Auto-Locate',
+            handler: () => {
+              this.locateUser();
+            }
+          },
+          {
+            text: 'Pick on Map',
+            handler: () => {
+              this.openMap();
+            }
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel'
+          }
+        ]
+      }
+    ).then(actionSheetEl => {
+      actionSheetEl.present();
+    });
+  }
+
+  private locateUser() {
+    if (!Capacitor.isPluginAvailable('Geolocation')) {
+      this.showErrorAlert();
+      return;
+    }
+    this.isLoading = true;
+    Plugins.Geolocation.getCurrentPosition()
+    .then(geoPosition => {
+      const coordinates: Coordinates = {
+        lat: geoPosition.coords.latitude,
+        lng: geoPosition.coords.longitude
+      };
+      this.createPlace(coordinates.lat, coordinates.lng);
+      this.isLoading = false;
+    })
+    .catch(error => {
+      this.isLoading = false;
+      this.showErrorAlert();
+    })
+  }
+
+  private openMap() {
     this.modalCtrl.create(
       {component: MapModalComponent}
     ).then(modalEl => {
@@ -33,25 +85,11 @@ export class LocationPickerComponent implements OnInit {
         if (!modalData.data) {
           return;
         }
-        const pickedLocation: PlaceLocation = {
+        const coordinates: Coordinates = {
           lat: modalData.data.lat,
-          lng: modalData.data.lng,
-          address: null,
-          staticMapImageUrl: null
+          lng: modalData.data.lng
         };
-        this.isLoading = true;
-        this.getAddress(modalData.data.lat, modalData.data.lng)
-        .pipe(
-          switchMap(address => {
-            pickedLocation.address = address;
-            return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 17));
-          })
-        ).subscribe(staticMapImageUrl => {
-          pickedLocation.staticMapImageUrl = staticMapImageUrl;
-          this.selectedLocationImage = staticMapImageUrl;
-          this.isLoading = false;
-          this.locationPick.emit(pickedLocation);
-        });
+        this.createPlace(coordinates.lat, coordinates.lng);
       });
       modalEl.present();
     });
@@ -73,6 +111,42 @@ export class LocationPickerComponent implements OnInit {
     return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}
     &size=500x500&maptype=roadmap&markers=color:red%7Clabel:Place%7C${lat},${lng}
     &key=${environment.googleMapsApiKey}`;
+  }
+
+  private createPlace(lat: number, lng: number) {
+    const pickedLocation: PlaceLocation = {
+      lat: lat,
+      lng: lng,
+      address: null,
+      staticMapImageUrl: null
+    };
+    this.isLoading = true;
+        this.getAddress(lat, lng)
+        .pipe(
+          switchMap(address => {
+            pickedLocation.address = address;
+            return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 17));
+          })
+        ).subscribe(staticMapImageUrl => {
+          pickedLocation.staticMapImageUrl = staticMapImageUrl;
+          this.selectedLocationImage = staticMapImageUrl;
+          this.isLoading = false;
+          this.locationPick.emit(pickedLocation);
+        });
+  }
+
+  private showErrorAlert() {
+    this.alertCtrl.create(
+      {
+        header: 'Could not fetch location...',
+        message: 'Try again later...',
+        buttons: [
+          'Okay'
+        ]
+      }
+    ).then(alertEl => {
+      alertEl.present();
+    })
   }
 
 }
